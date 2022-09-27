cf281535aa91f6fc7e10913b9bfbeddefdddd30b
Um Scottie <samuraimangu@SamuraiangusMBP.lan>
2022-09-27 17:08:50 -0400

    final commit, going to optimize the page with babel and webpack

--- src/script.js
+++ src/script.js
@@ -52,42 +52,42 @@ filtersEl.addEventListener('click', e => {
     projectEl1.style.transform = 'scale(0)';
     projectEl2.style.opacity = '0';
     projectEl2.style.transform = 'scale(0)';
-    projectEl5.style.opacity = '0';
-    projectEl5.style.transform = 'scale(0)';
+    projectEl4.style.opacity = '0';
+    projectEl4.style.transform = 'scale(0)';
     projectEl6.style.opacity = '0';
     projectEl6.style.transform = 'scale(0)';
 
     projectEl3.style.transform = 'translateX(-650px)';
     projectEl3.style.opacity = '1';
-    projectEl4.style.transform = 'translate(325px, -502px)';
-    projectEl4.style.opacity = '1';
+    projectEl5.style.transform = 'translate(20px, -355px)';
+    projectEl5.style.opacity = '1';
   } else if (filterText === 'Back end') {
+    projectEl3.style.opacity = '0';
+    projectEl3.style.transform = 'scale(0)';
+    projectEl4.style.opacity = '0';
+    projectEl4.style.transform = 'scale(0)';
+    projectEl5.style.opacity = '0';
+    projectEl5.style.transform = 'scale(0)';
+    projectEl6.style.opacity = '0';
+    projectEl6.style.transform = 'scale(0)';
+
+    projectEl1.style.transform = 'scale(1)';
+    projectEl1.style.opacity = '1';
+    projectEl2.style.transform = 'scale(1)';
+    projectEl2.style.opacity = '1';
+  } else if (filterText === 'Emails') {
     projectEl1.style.opacity = '0';
     projectEl1.style.transform = 'scale(0)';
     projectEl2.style.opacity = '0';
     projectEl2.style.transform = 'scale(0)';
     projectEl3.style.opacity = '0';
     projectEl3.style.transform = 'scale(0)';
-    projectEl4.style.opacity = '0';
-    projectEl4.style.transform = 'scale(0)';
-
-    projectEl5.style.transform = 'translate(-325px, -359px)';
-    projectEl5.style.opacity = '1';
-    projectEl6.style.transform = 'translate(-325px, -501px)';
-    projectEl6.style.opacity = '1';
-  } else if (filterText === 'Emails') {
-    projectEl3.style.opacity = '0';
-    projectEl3.style.transform = 'scale(0)';
-    projectEl4.style.opacity = '0';
-    projectEl4.style.transform = 'scale(0)';
     projectEl5.style.opacity = '0';
     projectEl5.style.transform = 'scale(0)';
-    projectEl6.style.opacity = '0';
-    projectEl6.style.transform = 'scale(0)';
 
+    projectEl4.style.transform = 'translateY(-500px)';
     projectEl4.style.opacity = '1';
-    projectEl4.style.transform = 'scale(1)';
+    projectEl6.style.transform = 'translate(-325px, -501px)';
     projectEl6.style.opacity = '1';
-    projectEl6.style.transform = 'scale(1)';
   }
 });
